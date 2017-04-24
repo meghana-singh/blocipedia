83579ff7cd1626fd27d706bbbb5e011db9e20bb2
Meghana Singh <meghana1602@gmail.com>
2017-04-24 20:30:43 +0000

    Completed Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,17 +19,15 @@ module ApplicationHelper
       link_attributes: { rel: 'nofollow', target: "_blank" },
       space_after_headers: true,
       fenced_code_blocks: true
-    }
-
+    }  
+    
     extensions = {
       autolink:           true,
       superscript:        true,
       disable_indented_code_blocks: true
     }
-
     renderer = Redcarpet::Render::HTML.new(options)
     markdown = Redcarpet::Markdown.new(renderer, extensions)
-
     markdown.render(text).html_safe
    end
   
