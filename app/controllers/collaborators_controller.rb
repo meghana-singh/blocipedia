9c30ff2d4cc932052afba1dc682cf133afe6e820
Meghana Singh <meghana1602@gmail.com>
2017-05-03 17:43:45 +0000

    Completed checkpoint-10

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,12 +1,15 @@
 class CollaboratorsController < ApplicationController
     def new
-      @collaborator = Collaborator.new
+      @collaborator  = Collaborator.new
+      @collaborators = Collaborator.all
       
       @wiki = Wiki.find(params[:wiki_id])
       
-      @collaborating_users = @wiki.collaborators.map { |colab| colab.user }           
+      @collaborating_users   = @wiki.collaborators.map { |colab| colab.user }           
+      @current_collaborators = @wiki.collaborators
       
       @users_currently_not_collaborating = User.where.not(id: @collaborating_users.map {|user| user.id})
+      @currently_not_collaborators = Collaborator.where.not(id: @current_collaborators.map {|colab| colab.id}) 
       
     end
     
@@ -26,18 +29,14 @@ class CollaboratorsController < ApplicationController
     end
     
     def destroy
-      wiki_id  = params[:wiki_id]
-      colab_id = params[:user_id]
+      collaborator = Collaborator.find(params[:id])
       
-      collaborator = Collaborator.find_by(wiki_id: wiki_id, user_id: colab_id)
-      puts collaborator
-=begin
       if collaborator.destroy
         flash[:notice] = "Colaborator #{collaborator.user.email} was removed from wiki."
       else
         flash[:alert] = " Collaborator #{collaborator.user.email} could not be removed."
       end
-=end
+
       
       redirect_to new_wiki_collaborator_path
     end
