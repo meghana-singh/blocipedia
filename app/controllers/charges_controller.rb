022e8b4284dea41af25ac5280bfb5daead6cdad3
Meghana Singh <meghana1602@gmail.com>
2017-10-24 18:52:43 +0000

    Used Stripe API to collect 100 charges

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,11 +1,38 @@
 class ChargesController < ApplicationController
+  
   def new
-    @amount = Amount.new(10_00)
+    @amount = Amount.new(50_00)  
+    
     @stripe_btn_data = {
-     key: "#{ Rails.configuration.stripe[:publishable_key] }",
-     description: "BigMoney Membership - #{current_user.email}",
-     amount: @amount.default
+      key: "#{ Rails.configuration.stripe[:publishable_key] }",
+      description: "BigMoney Membership - #{current_user.email}",
+      amount: Amount.default_amount
     }
+    
+    customer_charges_array = []
+    customer_ids           = []
+    customer_amounts       = []
+    
+    #Grab the first 100 charges by setting the limit to 100 (Default is 10)
+    charges = Stripe::Charge.list(:limit => 10)
+    charges.each do |charge|
+      customer_ids     << charge.id
+      customer_amounts << charge.amount
+    end
+    
+    #Use the has_more boolean to check if there are more charges.
+    #Use starting_after argument inorder to fetch the next page in the list.
+    while charges.has_more do
+      charges = Stripe::Charge.list(:limit => 10, :starting_after => charges.data.last.id)
+      charges.each do |charge|
+        customer_ids     << charge.id
+        customer_amounts << charge.amount
+      end
+    end
+    
+    puts customer_amounts
+    puts customer_ids
+    
   end
 
   def create
@@ -15,17 +42,17 @@ class ChargesController < ApplicationController
       return redirect_to root_path
     end
     
-    @amount = Amount.new(10_00)
     # Creates a Stripe Customer object, for associating with the charge
     customer = Stripe::Customer.create(
       email: current_user.email,
       card: params[:stripeToken]
     )
- 
+    puts Amount.default_amount
+    puts "--------"
     # Where the real magic happens
     charge = Stripe::Charge.create(
       customer: customer.id, # Note -- this is NOT the user_id in your app
-      amount: @amount.default,
+      amount: Amount.default_amount,
       description: "BigMoney Membership - #{current_user.email}",
       currency: 'usd'
     )
--- app/models/amount.rb
+++ app/models/amount.rb
@@ -1,11 +1,11 @@
 class Amount 
-    attr_accessor :def_amount
+    #attr_accessor :def_amount
     
     def initialize (def_amount)
-      @def_amount = def_amount
+      @@def_amount = def_amount
     end
     
-    def default 
-       return @def_amount
+    def self.default_amount 
+       @@def_amount
     end
 end
